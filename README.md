4d83ae99c484062508c59a61f1a9cc97ca0a38de
Alan Shaw <alan@freestyle-developments.co.uk>
2013-08-06 09:47:16 +0100

    OMG tabs!

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![nodeit logo](https://raw.github.com/alanshaw/nodeit/master/design/icon_128x128.png)
+
 # nodeit [![experimental](http://hughsk.github.io/stability-badges/dist/experimental.svg)](http://github.com/hughsk/stability-badges)
 
 > NodeJS text editor
@@ -15,3 +17,8 @@ Build:
 ```sh
 browserify js/main.js -o js/bundle.js
 ```
+
+## Credits
+
+* [Icons](http://www.wpzoom.com/wpzoom/new-freebie-wpzoom-developer-icon-set-154-free-icons/)
+* [Chrome Tabs](https://github.com/adamschwartz/chrome-tabs)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var createEditor = require('javascript-editor')
 
 var editor = createEditor({ container: document.querySelector('#editor') })
+  , $tabs = $("#tabs")
   , $editor = $("#editor")
 
 editor.on('change', function() {
@@ -11,8 +12,32 @@ editor.on('valid', function(noErrors) {
   // noErrors is a boolean
 })
 
+var $chromeTabsExampleShell = $tabs
+
+chromeTabs.init({
+  $shell: $chromeTabsExampleShell,
+  minWidth: 45,
+  maxWidth: 180
+});
+
+chromeTabs.addNewTab($chromeTabsExampleShell, {
+  //favicon: 'img/icon-doc.svg',
+  title: 'untitled',
+  data: {
+    timeAdded: +new Date()
+  }
+})
+
+$chromeTabsExampleShell.bind('chromeTabRender', function(){
+  var $currentTab = $chromeTabsExampleShell.find('.chrome-tab-current');
+  if ($currentTab.length && window['console'] && console.log) {
+    console.log('Current tab index', $currentTab.index(), 'title', $.trim($currentTab.text()), 'data', $currentTab.data('tabData').data);
+  }
+})
+
+
 function onResize () {
-  $editor.height($(window).height())
+  $editor.height($(window).height() - $tabs.outerHeight())
 }
 
 $(window).resize(onResize).resize()
