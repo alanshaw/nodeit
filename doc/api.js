9e20b0d31ca56af1475d809d7172ac891de466b7
Alan Shaw <alan@freestyle-developments.co.uk>
2013-08-24 09:26:10 +0100

    Updates

---
+++ doc/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Nodeit",
+        "bridge",
+        "codemirror",
+        "nodeit (module)"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
